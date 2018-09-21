50540a67de1af3c310d4b54f20041dc4c1b9e8fe
Ada 2018 <ada2018@10-45-48-141.0200.ifce.br.iosda.org>
2018-09-21 08:31:22 -0300

    Initial Commit

---
+++ BTC-Checker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BTC-Checker
+//
+//  Created by Ada 2018 on 21/09/2018.
+//  Copyright © 2018 tavaboy+daniboy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
